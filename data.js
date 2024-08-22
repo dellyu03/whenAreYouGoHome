70ce7372ffae7ebdc7f61fab6ad59a7806e83c8e
dellyu03 <seunghwan081@gmail.com>
2024-08-22 21:15:07 +0900

    resigster commands

--- data.js
+++
@@ -1,6 +0,0 @@
-export var userData = [
-    {
-        userName : "승환",
-        retire : "2024-08-13"
-    }
-]
--- index.js
+++ index.js
@@ -1,52 +1,16 @@
-import { REST, Routes } from 'discord.js';
-import { appConfig } from './config.js';
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const TOKEN = appConfig.token;
-const CLIENT_ID = appConfig.applicationID;
-
-const commands = [
-    {
-        name: 'ping',
-        description: 'Replies with Pong!',
-    },
-    {
-        name: 'home',
-        description: 'Greets the user with a hello message.',
-    },
-];
-
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-    console.error(error);
-}
-
-import { Client, GatewayIntentBits } from 'discord.js';
+// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const { commandName } = interaction;
-
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'home') {
-        await interaction.reply('2024-08-13!');
-    }
-});
-
-client.login(TOKEN);
-
-
-
+// Log in to Discord with your client's token
+client.login(token);
